fa6378748c6352068ef00ed153bf4ead942430b1
viborotto <vittoriaborotto@gmail.com>
2023-08-12 16:18:51 -0300

    Alterando shape da cobra para circulo

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,6 @@ module Main where
 import Snake
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
-import Debug.Trace
-import Data.Monoid
 
 data GameMessage = GameOverMessage Picture | TryAgainMessage Picture
 
@@ -18,7 +16,8 @@ instance Monoid GameMessage where
     mempty = GameOverMessage blank
 
 window :: Display
-window = InWindow "Haskell Snake Game" (640, 480) (100, 100)
+window = InWindow "Snakell Game" (640, 480) (100, 100)
+        -- FullScreen
 
 -- Carrega a imagem de fundo
 loadBackgroundImage :: IO Picture
@@ -30,8 +29,16 @@ background = makeColorI 135 206 235 255
 pipemarioColor :: Color
 pipemarioColor = makeColorI 0 128 0 255
 
+loadSnakeImage :: IO Picture
+loadSnakeImage = loadBMP "star.bmp"
+
+drawCircle :: Color -> (Float, Float) -> Float -> Picture
+drawCircle color' (tx, ty) radius = color color' $
+                                     translate (tx * 20 - 320) (ty * 20 - 240) $
+                                     circleSolid radius
+
 render :: GameState -> Picture
-render gameState = pictures $ shapes ++ fmap (convertToPicture black) snake ++
+render gameState = pictures $ shapes ++ fmap (convertToPicture yellow) snake ++
                               fmap (convertToPicture blue) [food] ++
                               [foldr mappend mempty gameOverMessage]
   where snake = getSnake gameState
@@ -41,7 +48,7 @@ render gameState = pictures $ shapes ++ fmap (convertToPicture black) snake ++
                  , fillRectangle pipemarioColor (0, 12) (20, 480)
                  , fillRectangle pipemarioColor (32, 12) (20, 480) ]
         convertToPicture :: Color -> (Int, Int) -> Picture
-        convertToPicture color' (x, y) = fillRectangle color' (toFloat (x, y)) (20, 20)
+        convertToPicture color' (x, y) = drawCircle color' (toFloat (x, y)) 10
         fillRectangle color' (tx, ty) (w, h) = color color' $
                                                 scale 1 (-1) $
                                                 translate (tx * 20 - 320) (ty * 20 - 240) $
@@ -77,12 +84,16 @@ update seconds gameState =  if (gameOver)
 handleKeys :: Event -> GameState -> GameState
 handleKeys (EventKey (SpecialKey KeyLeft ) Down _ _) gameState = changeDirection gameState LEFT
 handleKeys (EventKey (SpecialKey KeyRight) Down _ _) gameState = changeDirection gameState RIGHT
-handleKeys (EventKey (SpecialKey KeyUp   ) Down _ _) gameState = changeDirection gameState UP
-handleKeys (EventKey (SpecialKey KeyDown ) Down _ _) gameState = changeDirection gameState DOWN
+handleKeys (EventKey (SpecialKey KeyUp   ) Down _ _) gameState = changeDirection gameState DOWN
+handleKeys (EventKey (SpecialKey KeyDown ) Down _ _) gameState = changeDirection gameState UP
 handleKeys (EventKey (SpecialKey KeySpace) Down _ _) gameState =    if (isGameOver gameState)
                                                                     then initialGameState False
                                                                     else gameState
 handleKeys _ gameState = gameState
+
+
+figura :: String
+figura = "star.bmp"
 
 main :: IO ()
 main = play window background 10 (initialGameState True) render handleKeys update
